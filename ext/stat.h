fb7a2d7aa0c14c825b5619f921ba8af7583304c9
Christian Freitag <cfreitag.cf@googlemail.com>
2023-07-05 19:58:45 +0200

    - Added yet empty stat functions

---
+++ ext/stat.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2022 - 2023 bolthur project.
+//
+// This file is part of bolthur/bfs.
+//
+// bolthur/bfs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// bolthur/bfs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with bolthur/bfs.  If not, see <http://www.gnu.org/licenses/>.
+
+/** @file ext/stat.h */
+
+#include <sys/stat.h>
+#include <ext/type.h>
+
+#ifndef _EXT_STAT_H
+#define _EXT_STAT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ext_stat( const char* path, struct stat *st );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
